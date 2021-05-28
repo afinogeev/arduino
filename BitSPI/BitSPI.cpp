33dd9b48226d8525869ef18b1c2bb65559e62bcb
afinogeev <afinogeevrv@gmail.com>
2021-05-28 10:55:52 +0300

    serial deleted

--- BitSPI/BitSPI.cpp
+++ BitSPI/BitSPI.cpp
@@ -28,12 +28,8 @@ void BitSPI::end() {
 byte* BitSPI::transfer(byte* data, byte sizeBit = 8, byte startBit = 0) {
     byte* out = 0;
 	
-	Serial.println(*data, DEC);
-	Serial.println(sizeBit, DEC);
 	byte quantityByte = (sizeBit-1+startBit)/8+1u;
-	Serial.println(quantityByte, DEC);
 	byte stopBit = quantityByte>1?(sizeBit-1+startBit)%8:(sizeBit-1+startBit);
-	Serial.println(stopBit, DEC);
 	byte countBit = 0;
 	
 
@@ -43,23 +39,16 @@ byte* BitSPI::transfer(byte* data, byte sizeBit = 8, byte startBit = 0) {
 		byte shift = ibyte?0:startBit;
 		byte stop = ((ibyte+1)==quantityByte)?stopBit:7;
 		
-		Serial.println(shift, DEC);
-		Serial.println(stop, DEC);
-		Serial.println(curByte, DEC);
-		
 		for(byte ibit = shift; ibit<=stop; ibit++)
 			sendBit(curByte, ibit);
 		digitalWrite(_mosi, LOW);
     }
 	
-	Serial.println();
-	Serial.println(1111111, DEC);
     return out;
 }
 
 void BitSPI::sendBit(byte value, byte bit) {
 	
-	Serial.print(bitRead(value, 7-bit), BIN);
 	digitalWrite(_mosi, bitRead(value, 7-bit));
 	delayMicroseconds(quant);
 	digitalWrite(_sck, HIGH);
